2867812d97ba84bd20579f820f3e1b6a44acae93
Snitsaryuk Roman <nits14@mail.ru>
2022-04-11 14:29:04 +0300

    initial commit

---
+++ src/main/java/ru/sixzr/config/SecurityConfig.java
@@ -0,0 +1,45 @@
+package ru.sixzr.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+//  @Autowired
+//  public UserService userService;
+
+  @Bean
+  public PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
+  }
+
+//  @Autowired
+//  public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+//    auth.userDetailsService(userService).passwordEncoder(passwordEncoder());
+//  }
+
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
+    http.csrf().disable();
+
+    http.authorizeRequests()
+            .antMatchers("/register").permitAll()
+            .antMatchers("/profile").authenticated()
+            .and()
+            .formLogin()
+            .loginPage("/login")
+            .defaultSuccessUrl("/profile")
+            .usernameParameter("email")
+            .passwordParameter("password")
+            .failureUrl("/login?error").permitAll();
+  }
+}
