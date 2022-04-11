22feb276a4209199ce06a1d80a3c51b045dfe9a4
Snitsaryuk Roman <nits14@mail.ru>
2022-04-11 20:53:12 +0300

    added login view

--- src/main/java/ru/sixzr/controllers/LoginController.java
+++ src/main/java/ru/sixzr/controllers/LoginController.java
@@ -7,6 +7,7 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestParam;
+import ru.sixzr.models.forms.LoginForm;
 
 @Controller
 public class LoginController {
