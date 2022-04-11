d54b3b5f5d28584d5d7b1a0d15da0a462bbb8a50
Snitsaryuk Roman <nits14@mail.ru>
2022-04-11 15:03:02 +0300

    added static mapping for resources and pages

--- src/main/java/ru/sixzr/config/WebConfig.java
+++ src/main/java/ru/sixzr/config/WebConfig.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
@@ -19,6 +20,13 @@ public class WebConfig implements WebMvcConfigurer {
   @Override
   public void addResourceHandlers(ResourceHandlerRegistry registry) {
     registry.addResourceHandler("/css/**").addResourceLocations("/WEB-INF/assets/css/");
+    registry.addResourceHandler("/js/**").addResourceLocations("/WEB-INF/assets/js/");
+    registry.addResourceHandler("/img/**").addResourceLocations("/WEB-INF/assets/img/");
+  }
+
+  @Override
+  public void addViewControllers(ViewControllerRegistry registry) {
+    registry.addViewController("/about").setViewName("static/about");
   }
 
   @Bean
