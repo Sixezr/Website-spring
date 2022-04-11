e38cb32185fb17889de192706643c857986d754c
Snitsaryuk Roman <nits14@mail.ru>
2022-04-11 20:50:36 +0300

    added LoginController

---
+++ src/main/java/ru/sixzr/controllers/LoginController.java
@@ -0,0 +1,20 @@
+package ru.sixzr.controllers;
+
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class LoginController {
+
+    @GetMapping
+    @PreAuthorize("isAnonymous()")
+    public String login(@RequestParam(required = false) String error, @ModelAttribute("loginForm") LoginForm loginForm, BindingResult result, ModelMap map) {
+        map.put("error", error);
+        return "security/login";
+    }
+}
