f463fe18ea0f2d245e40f65dd601106ffdb68172
Snitsaryuk Roman <nits14@mail.ru>
2022-05-28 13:42:37 +0300

    added nested sql

--- src/main/java/ru/sixzr/controllers/ErrorHandlerController.java
+++ src/main/java/ru/sixzr/controllers/ErrorHandlerController.java
@@ -8,26 +8,26 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.http.HttpServletRequest;
 
-//@Controller
-//public class ErrorHandlerController implements ErrorController {
-//
-//    @RequestMapping(value = "/error")
-//    public String error(HttpServletRequest request) {
-//        Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
-//
-//        if (status != null) {
-//            int statusCode = Integer.parseInt(status.toString());
-//
-//            if (statusCode == HttpStatus.NOT_FOUND.value()) {
-//                return "error/not_found";
-//            }
-//        }
-//
-//        return "error/unknown";
-//    }
-//
-//    @Override
-//    public String getErrorPath() {
-//        return null;
-//    }
-//}
+@Controller
+public class ErrorHandlerController implements ErrorController {
+
+    @RequestMapping(value = "/error")
+    public String error(HttpServletRequest request) {
+        Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+
+        if (status != null) {
+            int statusCode = Integer.parseInt(status.toString());
+
+            if (statusCode == HttpStatus.NOT_FOUND.value()) {
+                return "error/not_found";
+            }
+        }
+
+        return "error/unknown";
+    }
+
+    @Override
+    public String getErrorPath() {
+        return null;
+    }
+}
